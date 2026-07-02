dab478eb8c73755af077d17ca76c944f5028b30e
Luqmanbrats <fannyskylark1659@gmail.com>
2026-07-02 15:34:09 +0800

    lab03

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte * 0.008
+gigabyte = kilobyte * 125000
+terabyte = kilobyte * 9.31323E-10
+petabyte = kilobyte * 9.1E-13
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
