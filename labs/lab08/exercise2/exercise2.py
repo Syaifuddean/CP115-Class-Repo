3be3b2abc93921b86bbaefcd9f6ac294cb1575b2
Syaifuddean <muhammadsyaifuddean@gmail.com>
2026-09-15 11:31:12 +0800

    lab 8

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -4,6 +4,26 @@ overtime_hours = int(input())
 tax_status = input()
 
 
+overtime_pay = overtime_hours * 35
+gross = base_salary + overtime_pay
+
+
+if tax_status == "Single" and gross >= 5000 :
+    tax_rate = 0.22
+elif tax_status == "Single" and gross < 5000 :
+    tax_rate = 0.18
+elif tax_status == "Married" and gross >= 6000 :
+    tax_rate = 0.20
+elif tax_status == "Married" and gross < 6000 :
+    tax_rate = 0.15
+elif tax_status == "Head" and gross >= 5500 :
+    tax_rate = 0.25
+else :
+    tax_rate = 0.19
+
+net_salary = gross - (gross * tax_rate) - (gross * 0.11) - (gross * 0.005) 
+
+
 
 print(employee_name)
 print(tax_rate)
--- labs/lab08/exercise3/exercise3.py
+++ labs/lab08/exercise3/exercise3.py
@@ -2,6 +2,48 @@ day_type = input()
 show_time = int(input())
 customer_type = input()
 
+if day_type == "weekend" and show_time > 18 and customer_type == "Adult" :
+    base_price = 18 
+    final_price = 18 +3
+elif day_type == "weekend" and show_time > 18 and customer_type ==" Child" :
+    base_price = 12 
+    final_price = 12+3
+elif day_type == "weekend" and show_time > 18 and customer_type =="Senior" :
+    base_price = 15 
+    final_price = 15+3
+elif day_type == "weekday" and show_time > 18 and customer_type == "Adult" :
+    base_price = 15 
+    final_price = 15+3
+elif day_type == "weekday" and show_time > 18 and customer_type == " Child" :
+    base_price = 10
+    final_price = 10+3 
+elif day_type == "weekday" and show_time > 18 and customer_type =="Senior" :
+    base_price = 12 
+    final_price = 12+3
+elif day_type== "weekend" and show_time <18 and customer_type =="Adult" :
+    base_price = 18
+    final_price = 18
+elif day_type == "weekend" and show_time <18 and customer_type ==" Child" :
+    base_price = 12
+    final_price = 12
+elif day_type == "weekend" and show_time <18 and customer_type =="Senior" :
+    base_price = 15
+    final_price = 15
+elif day_type== "weekday" and show_time <18 and customer_type =="Adult" :
+    base_price = 15
+    final_price = 15
+elif day_type == "weekday" and show_time <18 and customer_type ==" Child" :
+    base_price = 10
+    final_price = 10
+else :
+    base_price = 12
+    final_price = 12
+
+
+
+
+
+
 
 
 print(base_price)
